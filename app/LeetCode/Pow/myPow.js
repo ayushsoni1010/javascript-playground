d0f5ff6ce8788e21f061b755c4aba6b2bcf4ddac
Ayush Soni <mr.soni1002001@gmail.com>
2022-07-16 23:58:23 +0530

    update: added leetcode problems

---
+++ app/LeetCode/Pow/myPow.js
@@ -0,0 +1,12 @@
+// 50. Pow(x, n)
+
+// Q. Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function (x, n) {
+  return Math.pow(x, n);
+};
---
+++ app/LeetCode/Pow/myPow.ts
@@ -0,0 +1,12 @@
+// 50. Pow(x, n)
+
+// Q. Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+function myPowTS(x: number, n: number): number {
+  return Math.pow(x, n);
+}
