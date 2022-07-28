6d97dba06def5b3ffe21fe1e509315e000500b18
Ayush Soni <mr.soni1002001@gmail.com>
2022-07-28 23:42:28 +0530

    feat: search insert position problem (#14)

---
+++ code/LeetCode/SearchInsertPosition/searchInsert.ts
@@ -0,0 +1,11 @@
+// 35. Search Insert Position
+
+// Q. Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+function searchInsertTS(nums: number[], target: number): number {
+  nums.push(target);
+  nums.sort((a, b) => a - b);
+  return nums.indexOf(target);
+}
